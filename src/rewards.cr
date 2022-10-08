54e03a25936af76c7afc86fb09d8f8968b0204cf
BinuDR <36463075+BinuDR@users.noreply.github.com>
2022-10-08 09:44:07 -0500

    rewards - Added printing of reward line
    It will now print the line it matched from the server, and if the account had already been logged in it will send "boost" to the game and print the current login streak.

--- src/rewards.cr
+++ src/rewards.cr
@@ -32,9 +32,10 @@ module Rewards
       break if game.closed?
       next if from_game.nil? || from_game.strip.empty?
       break if from_game =~ /Thank you for logging into Dragonrealms/
-      break if from_game =~ /^<prompt/
+      break if from_game =~ /Current login streak/
+      game << "boost\n" if from_game =~ /^<prompt/
     end
     server.close
-    puts "%s> logged in" % [character.ljust(15)]
+    puts "%s> logged in - #{from_game}" % [character.ljust(15)]
   end
 end
